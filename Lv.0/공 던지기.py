2f797d25bcd11115dbad5dbfb4229f98ec2403fc
A Hyung Kim <125522834+gnuyhaa@users.noreply.github.com>
2023-09-04 21:48:50 +0900

    Create 공 던지기.py

---
+++ Lv.0/공 던지기.py
@@ -0,0 +1,7 @@
+# 공은 1번부터 던지며 오른쪽으로 한 명을 건너뛰고 그다음 사람에게만 던질 수 있습니다. 친구들의 번호가 들어있는 정수 배열 numbers와 정수 K가 주어질 때, k번째로 공을 던지는 사람의 번호는 무엇인지 return 하도록 solution 함수를 완성해보세요.
+
+def solution(numbers, k):
+    idx = 0
+    for i in range(k-1):
+        idx = (idx +2) % len(numbers)
+    return numbers[idx]
