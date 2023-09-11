d8d43a3fb2694cb8992818f28c021c6fd66066a5
A Hyung Kim <125522834+gnuyhaa@users.noreply.github.com>
2023-09-11 16:34:51 +0900

    Create 모음 제거.py

---
+++ Lv.0/모음 제거.py
@@ -0,0 +1,7 @@
+# 영어에선 a, e, i, o, u 다섯 가지 알파벳을 모음으로 분류합니다. 문자열 my_string이 매개변수로 주어질 때 모음을 제거한 문자열을 return하도록 solution 함수를 완성해주세요.
+
+def solution(my_string):
+    col = ("a,e,i,o,u")
+    for i in col:
+        my_string = my_string.replace(i,"")
+    return my_string
