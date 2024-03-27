662ec9595c5b78dd372e9164e446c82cd72dd19b
A Hyung Kim <125522834+gnuyhaa@users.noreply.github.com>
2024-03-27 14:27:20 +0900

    Update 소수찾기.py

--- Lv.1/소수찾기.py
+++ Lv.1/소수찾기.py
@@ -1,8 +1,13 @@
 import math
+def isPrime(n):
+    for i in range(2, int(math.sqrt(n))+1):
+        if n % i == 0:
+            return False
+    return True
 
-# 소수 판별
-def primenumber(x):
-    for i in range (2, int(math.sqrt(x) + 1):	# 2부터 x의 제곱근까지의 숫자
-    	if x % i == 0:		# 나눠떨어지는 숫자가 있으면 소수가 아님
-        	return False
-    return True		
+def solution(n):
+    answer = 0
+    for i in range(2, n+1):
+        if isPrime(i) == True:
+            answer += 1
+    return answer
