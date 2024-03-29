690e071022ee39fdbda2d4cecbf8beb8a4a7b586
A Hyung Kim <125522834+gnuyhaa@users.noreply.github.com>
2024-03-30 00:01:10 +0900

    Update 실패율.py

--- Lv.1/실패율.py
+++ Lv.1/실패율.py
@@ -1,26 +1,18 @@
-def solution(N, stages): # 실패율 O(n**2)
+def solution(N, stages): 
     answer = []
-    fail_list=[]
+    sum_list=[0]*(N+2)
     dic_list={}
-    answer_list=[]
+    stages.sort()
+    now=len(stages)
+    for i in stages:
+        sum_list[i]+=1
+        
+
     for i in range(1, N+1):
-        full=0
-        reach=0
-        for j in stages:
-            if j >= i:
-                full+=1
-            if j == i:
-                reach+=1
-        if full==0:
-            dic_list[i]=0
-        else:            
-            dic_list[i]=reach/full 
-
-    # return dic_list
-    
-    dict1=sorted(dic_list, key=lambda x : x[1], reverse=True)
-    
-    for i in dict1:
-        answer.append(i[0])
-
-    return answer
+        if now==0:
+            dic_list[i] = 0
+            continue
+        dic_list[i] = sum_list[i]/now
+        now-=sum_list[i]
+        
+    return sorted(dic_list, key=lambda x : dic_list[x], reverse=True)
