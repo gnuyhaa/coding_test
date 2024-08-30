c4c4401a19df68606539f5c21dd0d978e6dd2fe9
A Hyung Kim <125522834+gnuyhaa@users.noreply.github.com>
2024-08-30 11:14:44 +0900

    Update 숫자블록.py

--- Lv.2/숫자블록.py
+++ Lv.2/숫자블록.py
@@ -1,11 +1,22 @@
-def solution(begin, end):
-    answer = []
-    for i in range(begin, end+1):
-        k = int(i != 1)  # i가 1이면 0, 1이 아니면 1을 대입
-        for j in range(2, int(i**0.5)+1): # i**0.5 == sqrt(i)
-            if i%j == 0 and i//j <= 10000000:
-                k = i//j  # j가 2부터 커지기 때문에 처음 만나는 i//j가 약수 중 가장 큰 값
-                break
-        answer.append(k)
+def maxdivisor(n):
+    if n == 1:
+        return 0
     
+    divisors = []
+    for x in range(1, int(n**0.5) + 1):
+        if n % x == 0:
+            if x <= 1e7:
+                divisors.append(x)
+            if n // x <= 1e7 and n // x != n:
+                divisors.append(n//x)
+    
+    return max(divisors)
+
+def solution(begin, end):
+    # 규칙성을 파악해보면,
+    # 배열의 n번째 원소는 자기 자신이 아닌 가장 큰 약수가 됨을
+    # 알 수 있습니다.
+    # 주의) 10000000까지의 숫자가 적힌 블록들을 이용했으므로,
+    # 자기 자신이 아니면서, 10000000 이하의 가장 큰 약수를 구해야 합니다.
+    answer = [maxdivisor(n) for n in range(begin, end+1)]
     return answer
