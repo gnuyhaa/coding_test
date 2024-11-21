3a3415d2c8b85d75842643ba73e821b56293ae43
A Hyung Kim <125522834+gnuyhaa@users.noreply.github.com>
2024-11-21 17:56:22 +0900

    Update 간단한 소인수분해.py

--- SW Expert Academy/간단한 소인수분해.py
+++ SW Expert Academy/간단한 소인수분해.py
@@ -1,7 +1,6 @@
 T = int(input())
 nums = [2, 3, 5, 7, 11]
-# 여러개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
-for test_case in range(1, T + 1):
+for tc in range(1, T + 1):
     temp = {2: 0, 3: 0, 5: 0, 7: 0, 11: 0} # [2^a, 3^b, 5^c, 7^d, 11^e] 카운트
     N = int(input())
     for i in nums:
@@ -12,6 +11,6 @@ for test_case in range(1, T + 1):
             else:
                 break
 
-    print("#{0} {1} {2} {3} {4} {5}".format(test_case, list(temp.values())[0],
+    print("#{0} {1} {2} {3} {4} {5}".format(tc, list(temp.values())[0],
                                             list(temp.values())[1], list(temp.values())[2],
                                             list(temp.values())[3], list(temp.values())[4],))
